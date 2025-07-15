117aef085b0d7ec8c36826d5145a9e5625aff8a0
Leul-dev2 <omegaseyoum@gmail.com>
2025-07-15 22:13:11 +0300

    Resolve merge

--- firebase.js
+++ firebase.js
@@ -4,15 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const serviceAccountJson = process.env.GOOGLE_SERVICE_ACCOUNT_KEY;
+
 if (!serviceAccountJson) {
   throw new Error('Missing GOOGLE_SERVICE_ACCOUNT_KEY in environment variables');
 }
 
 const serviceAccount = JSON.parse(serviceAccountJson);
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
-export default admin;
+const db = admin.firestore();
+
+export { admin, db };
